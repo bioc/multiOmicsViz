051d3ff1e6a23cd602afc2018f4c79d76ff24a1e
lshep <lori.shepherd@roswellpark.org>
2024-02-29 13:17:55 -0500

    Deprecate - unresponsive

--- R/zzz.R
+++ R/zzz.R
@@ -10,3 +10,11 @@ datacache <- new.env(hash=TRUE, parent=emptyenv())
     chromLength <- read.table(chromLength,header=FALSE,sep="\t",stringsAsFactors=FALSE)
     assign("chromLength", chromLength, envir=datacache)
 }
+
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.20")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
+
